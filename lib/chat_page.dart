845815275ef0c647abd9d4b9a753a8ea7410a71e
Shivam Raj <shivam1raj5@gmail.com>
2023-11-08 17:32:51 +0530

    Chat page created

---
+++ lib/chat_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({
+    required this.chatApi,
+    super.key,
+  });
+
+  final ChatApi chatApi;
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _messages = <ChatMessage>[
+    ChatMessage('Hello, how can I help?', false),
+  ];
+  var _awaitingResponse = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                ..._messages.map(
+                  (msg) => MessageBubble(
+                    content: msg.content,
+                    isUserMessage: msg.isUserMessage,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MessageComposer(
+            onSubmitted: _onSubmitted,
+            awaitingResponse: _awaitingResponse,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _onSubmitted(String message) async {
+    setState(() {
+      _messages.add(ChatMessage(message, true));
+      _awaitingResponse = true;
+    });
+    try {
+      final response = await widget.chatApi.completeChat(_messages);
+      setState(() {
+        _messages.add(ChatMessage(response, false));
+        _awaitingResponse = false;
+      });
+    } catch (err) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('An error occurred. Please try again.')),
+      );
+      setState(() {
+        _awaitingResponse = false;
+      });
+    }
+  }
+}
